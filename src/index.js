32dd589ea1ba3c9577fb46673c28baaca61de4cf
lucas machado <lucassteffen30@gmail.com>
2023-04-29 12:05:34 -0300

    botão

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import Botao from './Botao.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function App() {
+    const [showHello, setShowHello] = useState(false);
+
+    const handleClick = () => {
+        console.log('Clicou no botão!');
+        setShowHello(true);
+    };
+
+    return (
+        <div>
+            <h1>Clique no botão abaixo</h1>
+            <Botao onClick={handleClick} label="Botão" />
+            {showHello && <h2>Hello World</h2>}
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
